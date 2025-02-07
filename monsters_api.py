8820aecb90f850710f9873c73afeec4d37cc7230
Sadjaime <41895620+Sadjaime@users.noreply.github.com>
2025-02-07 15:09:46 +0100

    Update monsters_api.py

--- monsters_api.py
+++ monsters_api.py
@@ -61,4 +61,4 @@ def get_monsters():
     return jsonify(filtered_monsters)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
