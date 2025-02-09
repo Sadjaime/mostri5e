7a7f36cd60d055227c7bb4bf20b1a0b560453c32
Sadjaime <41895620+Sadjaime@users.noreply.github.com>
2025-02-09 02:17:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js (Production)
-export const API_BASE_URL = "https:mostri5e-production.up.railway.app:8080/api";
+export const API_BASE_URL = "https://mostri5e-production.up.railway.app:8080/api";
