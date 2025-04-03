d61d7d17b09bb23f1351df7300ea59e063880fa0
Sadjaime <sadtyrionsmurf@gmail.com>
2025-04-03 14:50:56 +0200

    Css for "li" and "ul"

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js (Production)
-export const API_BASE_URL = "https://mostri5e-production.up.railway.app/api";
+//export const API_BASE_URL = "https://mostri5e-production.up.railway.app/api";
+export const API_BASE_URL = "http://127.0.0.1:8080/api"
